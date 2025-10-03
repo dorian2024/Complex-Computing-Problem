e68013c0d3d5c4a5bb1c2c0ec8a3f12f59969e00
Umaima Zeeshan <157504892+dorian2024@users.noreply.github.com>
2025-10-03 11:46:47 +0500

    Update gprof2pdf.sh
    update command for profiling top contributing functions

--- src/V1/gprof2pdf.sh
+++ src/V1/gprof2pdf.sh
@@ -10,10 +10,13 @@ pdfFile=${filename}.pdf
 dotFile=${filename}.dot
 
 # for all functions
+#-e0 means show all edges regardless of how small their contribution is
 ./gprof2dot.py -e0 -n0 --skew=0.01 ${gprofFile} > $dotFile
 
 # for some top contributing functions
-# ./gprof2dot.py -e0 -n0 --skew=0.01 ${gprofFile} > $dotFile
+#the -n0.5 flag adds a threshold of 0.5 to the nodes 
+# only show functions whose total time fraction is at least 0.5% of the program’s runtime.
+# ./gprof2dot.py -e0 -n0.5 --skew=0.01 ${gprofFile} > $dotFile
 
 dot -Tpdf -o $pdfFile $dotFile
 
