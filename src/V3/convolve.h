3e565bd9c0cb77c2d7a874df977cd980ad34b3e6
Umaima Zeeshan <157504892+dorian2024@users.noreply.github.com>
2025-10-29 06:26:46 -0400

    Add files via upload

---
+++ src/V3/convolve.h
@@ -0,0 +1,44 @@
+/*********************************************************************
+ * convolve.h
+ *********************************************************************/
+
+#ifndef _CONVOLVE_H_
+#define _CONVOLVE_H_
+
+#include "klt.h"
+#include "klt_util.h"
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void _KLTToFloatImage(
+  KLT_PixelType *img,
+  int ncols, int nrows,
+  _KLT_FloatImage floatimg);
+
+void _KLTComputeGradients(
+  _KLT_FloatImage img,
+  float sigma,
+  _KLT_FloatImage gradx,
+  _KLT_FloatImage grady);
+
+void _KLTGetKernelWidths(
+  float sigma,
+  int *gauss_width,
+  int *gaussderiv_width);
+
+void _KLTComputeSmoothedImage(
+  _KLT_FloatImage img,
+  float sigma,
+  _KLT_FloatImage smooth);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
